4d54b7a02b190787b34fb6a731554e7834fac925
cagri <akbaba_dograma@hotmail.com>
2021-03-12 21:57:21 +0300

    FinalProject

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,34 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+using Newtonsoft.Json;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ProductTest();
+
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+
+            foreach (var item in categoryManager.GetAll())
+            {
+                Console.WriteLine(item.CategoryName);
+            }
+        }
+
+        private static void ProductTest()
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+
+            foreach (var product in productManager.GetByUnitPrice(50, 100))
+            {
+                Console.WriteLine(product.ProductName);
+            }
+        }
+    }
+}
+ 
---
+++ DataAccess/Concrete/EntityFramework/EfOrderDal.cs
@@ -0,0 +1,14 @@
+﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Abcstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+   public class EfOrderDal:EfEntityRepositoryBase<Order,NorthwindContext>,IOrderDal
+    {
+
+    }
+}
